9bce055afea79237afa7df1572d24919ec5e8a5f
Diego <dberriorueda@gmail.com>
2024-02-27 21:52:45 -0500

    Organizando pagina fron

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.getElementById('parkingForm').addEventListener('submit', function(event
 
     var vehicleType = document.getElementById('vehicleType').value;
     var entryTime = document.getElementById('entryTime').value;
+    var exitTime = document.getElementById('exitTime').value;
 
     var currentTime = new Date();
     var currentHour = currentTime.getHours();
@@ -11,19 +12,24 @@ document.getElementById('parkingForm').addEventListener('submit', function(event
     var entryHour = parseInt(entryTime.split(':')[0]);
     var entryMinute = parseInt(entryTime.split(':')[1]);
 
-    var totalMinutes = (currentHour - entryHour) * 60 + (currentMinute - entryMinute);
+    var exitHour = parseInt(entryTime.split(':')[0]);
+    var exitMinute = parseInt(exitTime.split(':')[1]);
+
+    var totalMinutes = (exitHour - entryHour) * 60 + (exitMinute - entryMinute);
+    var totalHours = Math.ceil(totalMinutes / 60);
 
     var totalCost;
 
     if (totalMinutes <= 10) {
         totalCost = 0;
     } else if (totalMinutes <= 180) { // Cambiado de totalHours a totalMinutes
-        totalCost = Math.ceil(totalMinutes / 60) * 2200;
+        totalCost = totalHours * 2200;
     } else {
         totalCost = 4000;
     }
 
     var outputMessage = '<p>Hora de entrada: ' + entryTime + '</p>' +
+                        '<p> Hora de Salida: ' + exitTime + '</p>' +
                         '<p>Hora transcurridas: ' + Math.ceil(totalMinutes / 60) + '</p>' + 
                         '<p>Total a pagar: $' + totalCost + '</p>';
 
