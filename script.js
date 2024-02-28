e1213ba224bc046cc6c4b14c3b7abc1c7ab53df4
Diego <dberriorueda@gmail.com>
2024-02-28 16:57:02 -0500

    Organizando la logica para el parqueadero

--- script.js
+++ script.js
@@ -2,20 +2,17 @@ document.getElementById('parkingForm').addEventListener('submit', function(event
     event.preventDefault();
 
     var vehicleType = document.getElementById('vehicleType').value;
+    var licensePlate = document.getElementById('licensePlate').value;
     var entryTime = document.getElementById('entryTime').value;
     var exitTime = document.getElementById('exitTime').value;
-
-    var currentTime = new Date();
-    var currentHour = currentTime.getHours();
-    var currentMinute = currentTime.getMinutes();
 
     var entryHour = parseInt(entryTime.split(':')[0]);
     var entryMinute = parseInt(entryTime.split(':')[1]);
 
-    var exitHour = parseInt(entryTime.split(':')[0]);
+    var exitHour = parseInt(exitTime.split(':')[0]);
     var exitMinute = parseInt(exitTime.split(':')[1]);
 
-    var totalMinutes = (exitHour - entryHour) * 60 + (exitMinute - entryMinute);
+    var totalMinutes = (exitHour * 60 + exitMinute) - (entryHour * 60 + entryMinute);
     var totalHours = Math.ceil(totalMinutes / 60);
 
     var totalCost;
@@ -28,9 +25,11 @@ document.getElementById('parkingForm').addEventListener('submit', function(event
         totalCost = 4000;
     }
 
-    var outputMessage = '<p>Hora de entrada: ' + entryTime + '</p>' +
+    var outputMessage = '<p>Numero de placa: ' + licensePlate + '</p>' +
+                        '<p> Tipo de Vehiculo: '+ vehicleType + '</p>' +
+                        '<p>Hora de entrada: ' + entryTime + '</p>' +
                         '<p> Hora de Salida: ' + exitTime + '</p>' +
-                        '<p>Hora transcurridas: ' + Math.ceil(totalMinutes / 60) + '</p>' + 
+                        '<p>Hora transcurridas: ' +totalHours + '</p>' + 
                         '<p>Total a pagar: $' + totalCost + '</p>';
 
     document.getElementById('output').innerHTML = outputMessage;
